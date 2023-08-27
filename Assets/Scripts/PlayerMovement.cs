af1dba30c82c53dc503bd621c4ae656796ac304f
Dima <DimaBrisset@gmail.com>
2023-08-27 20:20:11 +0300

    3
    3

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,170 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+
+
 
 
 public class PlayerMovement : MonoBehaviour
 {
 
-    public GameObject[] hearts;
+    [SerializeField] Rigidbody2D _rigidbody2D;
+    [SerializeField] Transform _groundCheck;
+    [SerializeField] LayerMask _groundLayer;
 
-    [SerializeField] private Transform _groundCheck;
-    [SerializeField] private LayerMask _whatIsGround;
-    [SerializeField] private float _extraJumpValue;
-    [SerializeField] private float _checkRadius;
-    [SerializeField] private float _jumpForce;
-    [SerializeField] private int _health;
+    [SerializeField] float _speed;
+    [SerializeField] float _jumpPower;
 
-    private Vector2 _moveInput;
-    private bool _facingRight = true;
-    private Rigidbody2D _rigidbody2D;
-    private SpriteRenderer _sprite;
-    private Animator _animator;
-    private InputAction _jumpAction;
+    private float _horizontal;
+    private bool isFacingRight = true;
 
-    private float _extraJump;
-    private float _speed = 5f;
-    private bool _isGround;
-   
-    private const int MAX_HEALTH = 3;
 
-   
 
-    private enum MovementState
+
+    // Update is called once per frame
+    void Update()
     {
-        idle,
-        running,
-        jumping,
-      
-    }
-
-
-    void Start()
-    {
-
-        _rigidbody2D = GetComponent<Rigidbody2D>();
-        _sprite = _rigidbody2D.GetComponent<SpriteRenderer>();
-        _health = MAX_HEALTH;
-        _animator = GetComponent<Animator>();
-
-         
-    }
-
-
-    private void Update()
-    {
-      
-
-        Move();
-        Flip();
-        UpdateAnimationState();
-    }
-
-    private void UpdateAnimationState()
-    {
-        MovementState state;
-
-        if (_rigidbody2D.velocity.y > .1f && _isGround == false)
+        _rigidbody2D.velocity = new Vector2(_horizontal * _speed, _rigidbody2D.velocity.y);
+        if (!isFacingRight && _horizontal > 0f)
         {
-            state = MovementState.jumping;
+            Flip();
         }
-        
-
-        else
+        else if (isFacingRight && _horizontal < 0f)
         {
-            switch (_moveInput.x)
-            {
-                case float x when x > 0f:
-                    state = MovementState.running;
-                    _sprite.flipX = false;
-                    break;
-                case float x when x < 0f:
-                    state = MovementState.running;
-                    Flip();
-                    break;
-                case float x when x == 0f:
-                    state = MovementState.idle;
-                    break;
-
-                default:
-                    state = MovementState.idle;
-                    break;
-            }
-        }
-
-        _animator.SetInteger("state", (int)state);
-    }
-
-
-
-
-    private void Move()
-    {
-        _moveInput.x = Input.GetAxis("Horizontal");
-        _rigidbody2D.velocity = new Vector2(_moveInput.x * _speed, _rigidbody2D.velocity.y);
-    }
-
-    private void Jump(InputAction.CallbackContext context)
-    {
-        _isGround = Physics2D.OverlapCircle(_groundCheck.position, _checkRadius, _whatIsGround);
-        if (_isGround == true)
-        {
-            _extraJump = _extraJumpValue;
-        }
-
-        if (context.performed  && _extraJump > 0)
-        {
-            _rigidbody2D.velocity = Vector2.up * _extraJump;
-            _extraJump--;
-        }
-
-        else if (context.performed && _extraJump == 0 && _isGround == true)
-        {
-            _rigidbody2D.velocity = Vector2.up * _jumpForce;
-        }
-
-
-    }
-
- /*   private async void OnCollisionEnter2D(Collision2D collision2D)
-    {
-        if (collision2D.gameObject.GetComponent<EnemyMovement>())
-        {
-
-            _health -= 1;
-            UpdateHearts();
-        }
-
-        if (_health < 1)
-        {
-            Destroy(gameObject);
-            await Task.Delay(3000);
+            Flip();
         }
     }
- */
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed && IsGround())
+        {
+            _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, _jumpPower);
+        }
+        if (context.canceled && _rigidbody2D.velocity.y > 0)
+        {
+            _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, _rigidbody2D.velocity.y * 0.5f);
+        }
+    }
+    private bool IsGround()
+    {
+        return Physics2D.OverlapCircle(_groundCheck.position, 0.2f, _groundLayer);
+    }
+
     private void Flip()
     {
-        if (_moveInput.x > 0 && _facingRight == false || _moveInput.x < 0 && _facingRight == true)
-        {
-            var transform1 = transform;
-            Vector3 theScale = transform1.localScale;
-            theScale.x *= -1f;
-            transform1.localScale = theScale;
-
-            _facingRight = !_facingRight;
-           // Debug.Log(_moveInput.x);
-        }
+        isFacingRight = !isFacingRight;
+        Vector3 localScale = transform.localScale;
+        localScale.x *= -1f;
+        transform.localScale = localScale;
     }
-
-    private void UpdateHearts()
+    public void Move(InputAction.CallbackContext context)
     {
-        for (int i = 0; i < hearts.Length; i++)
-        {
-            if (i < _health)
-                hearts[i].SetActive(true);
-            else
-                hearts[i].SetActive(false);
-        }
-    }
+        _horizontal = context.ReadValue<Vector2>().x;
 
+    }
 
 }
